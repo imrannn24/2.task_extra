ade12400f4b3dd24208cfd036441c5107c56b832
imrannn24 <imran200424@icloud.com>
2023-01-11 14:06:27 +0600

    dz

--- 2.task_extra/main.swift
+++ 2.task_extra/main.swift
@@ -1,11 +1,39 @@
-//
-//  main.swift
-//  2.task_extra
-//
-//  Created by imran on 07.01.2023.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+print("Введите первое число")
+if let chislo1 = readLine(){
+    
+    let a =  Int(chislo1) ?? 0
+print("Выберите действие: '+','-','*','/'")
+    
+if let deistvie = readLine(){
+        
+    let c = String(deistvie)
+        
+print("Введите второе число")
+if let chislo2 = readLine(){
+                
+    let b = Int(chislo2) ?? 0
+    if c == "-"{
+        print("\(a) - \(b) = \(a - b)")
+        }
+    else if c == "+"{
+        print("\(a) + \(b) = \(a + b)")
+        }
+    else if c == "*"{
+        print("\(a) * \(b) = \(a * b)")
+        }
+    else if c == "/"{
+        print("\(a) / \(b) = \(a / b)")
+            }
+    else {
+        print("error")
+    }
+        }
+   }
+    
+}
+    else{
+        print("Number format is incorrect")
+        
+    }
